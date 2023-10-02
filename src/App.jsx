b0c409bac57ead07376c76be624b26816ed6ee01
nicholassuwirman <wilsonsuwirmans@gmail.com>
2023-10-02 23:18:34 +0200

    Updated code config

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './containers/navbar/Navbar'
 import Hero from './containers/hero/Hero'
 import Product from './containers/product/product'
+
 function App() {
   return (
     <>

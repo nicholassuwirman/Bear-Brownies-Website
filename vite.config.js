a603f880f4a2f91c270baad674158807e4fba64a
nicholassuwirman <wilsonsuwirmans@gmail.com>
2023-10-02 23:11:37 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Bear-Brownies-Website/",
   plugins: [react()],
-  build: {
-    outDir: 'dist', // This will generate the build output in a 'dist' directory
-  },
 })
